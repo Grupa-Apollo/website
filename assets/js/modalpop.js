4fbf44c6184c0dabf8f4db3034fed28a1254a29f
Ronax <44991437+ronaxdevil@users.noreply.github.com>
2020-10-02 13:33:43 +0530

    optimize wait timing of preloader and modal popup

--- assets/js/modalpop.js
+++ assets/js/modalpop.js
@@ -15,5 +15,5 @@ $(window).on('load', function () {
     var search = '#' + helper.getDocumentHash(value);
     setTimeout(function () {
         $(search).modal('show');
-    }, 5000);
+    }, 4000);
 });
--- assets/js/preload.js
+++ assets/js/preload.js
@@ -1,15 +1,9 @@
- function wait(ms) {
-     var start = new Date().getTime();
-     var end = start;
-     while (end < start + ms) {
-         end = new Date().getTime();
-     }
- }
  window.addEventListener('load', function () {
      loding();
  });
 
  function loding() {
-     wait(4000);
-     document.querySelector('body').classList.add("loaded");
+     setTimeout(function () {
+         document.querySelector('body').classList.add("loaded");
+     }, 5000);
  }
