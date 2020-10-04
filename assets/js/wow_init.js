0c3a8f10a98610565828ac3e2502f9dadea8fb0d
Ronax <44991437+ronaxdevil@users.noreply.github.com>
2020-10-04 18:00:43 +0530

    Remove wow animations

--- assets/js/wow_init.js
+++
@@ -1,8 +0,0 @@
-wow = new WOW({
-    boxClass: "wow", // default
-    animateClass: "animate__animated", // default
-    offset: 0, // default
-    mobile: false, // default
-    live: true, // default
-});
-wow.init();
