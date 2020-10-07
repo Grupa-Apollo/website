309e97445fa275b0288512466ccc2aad716f4c8b
Ronax <44991437+ronaxdevil@users.noreply.github.com>
2020-10-07 05:49:05 +0530

    Add proper device page

--- assets/js/preload.js
+++
@@ -1,9 +0,0 @@
- window.addEventListener('load', function () {
-     loding();
- });
-
- function loding() {
-     setTimeout(function () {
-         document.querySelector('body').classList.add("loaded");
-     }, 1000);
- }
